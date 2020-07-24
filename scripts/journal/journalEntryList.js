b1fec2de04db5988566cf306122b642026ae741b
Chris Johnson <christopherjohnson1@gmail.com>
2020-07-24 11:05:19 -0500

    refactored the for...of loops to use the .map() and .join() methods

--- scripts/journal/journalEntryList.js
+++ scripts/journal/journalEntryList.js
@@ -5,14 +5,9 @@ export const journalList = () => {
   const contentElement = document.querySelector(".automated")
   const journals = useJournalEntries()
 
-  let journalHTMLrepresentation = ""
-  for(const journalObj of journals){
-      journalHTMLrepresentation += journalAsHTML(journalObj)
-  }
-
   contentElement.innerHTML += `
       <article class="journalEntriesList">
-          ${journalHTMLrepresentation}
+          ${journals.map(journalObj => journalAsHTML(journalObj)).join('')}
       </article>
   `
 }
